8243ecfc604162455f23f4ceb9cc249298cbdade
Raphael Mathias <oi.sucesso@gmail.com>
2025-05-29 15:52:38 -0300

    First commit

---
+++ src/main/java/com/github/chiarelli/curso_idiomas_api/CursoIdiomasApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.chiarelli.curso_idiomas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoIdiomasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoIdiomasApiApplication.class, args);
+	}
+
+}

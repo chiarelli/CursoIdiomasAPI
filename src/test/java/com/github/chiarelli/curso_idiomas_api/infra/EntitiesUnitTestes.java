55b1ecd226789fd4f1a686a6e35b6dd81a65c475
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-01 22:27:37 -0300

    Created unit tests for aluno and turma registration use cases

--- src/test/java/com/github/chiarelli/curso_idiomas_api/infra/EntitiesUnitTestes.java
+++ src/test/java/com/github/chiarelli/curso_idiomas_api/infra/EntitiesUnitTestes.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +14,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.TransactionSystemException;
 
+import com.github.chiarelli.curso_idiomas_api.escola.application.usecases.CadastrarAlunoUseCase;
+import com.github.chiarelli.curso_idiomas_api.escola.application.usecases.CadastrarTurmaUseCase;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.DomainException;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.commands.CadastrarNovaTurmaCommand;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.commands.RegistrarNovoAlunoCommand;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.contracts.AlunoInterface;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.contracts.TurmaInterface;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.model.Turma;
 import com.github.chiarelli.curso_idiomas_api.escola.infra.persistence.AlunoPersistence;
 import com.github.chiarelli.curso_idiomas_api.escola.infra.persistence.AlunoRepository;
+import com.github.chiarelli.curso_idiomas_api.escola.infra.persistence.TurmaRepository;
 
 @SpringBootTest
 public class EntitiesUnitTestes {
 
   @Autowired AlunoRepository alunoRepository;
+  @Autowired TurmaRepository turmaRepository;
 
   @Test
   void alunoEntitySalvarComDadosInvalidos() {
@@ -47,4 +59,263 @@ public class EntitiesUnitTestes {
     assertThrows(TransactionSystemException.class, () -> alunoRepository.save(alunoInvalid3)).getMessage();
     
   }
+
+  @Autowired CadastrarAlunoUseCase cadastrarAluno;
+  @Autowired CadastrarTurmaUseCase cadastrarTurma;
+
+  @Test
+  void cadastrarNovoAlunoSemTurma() {
+    // Prepare
+    alunoRepository.deleteAll();
+    turmaRepository.deleteAll();
+
+    AtomicReference<TurmaInterface> turma = new AtomicReference<>();
+    AtomicReference<AlunoInterface> aluno = new AtomicReference<>();
+
+    assertDoesNotThrow(() -> {
+      var t = cadastrarTurma.handle(
+        new CadastrarNovaTurmaCommand(
+          125,
+          2025
+      ));
+      turma.set(t);
+    });
+    // System.out.println("Turma: " + turma.get().toString());
+
+    assertDoesNotThrow(() -> {
+      var a = cadastrarAluno.handle(
+        new RegistrarNovoAlunoCommand(
+          "Aluno 1",
+          "998.180.620-00",
+          "8f6wZ@example.com",
+          Set.of(turma.get().getTurmaId())
+        )
+      );
+      aluno.set(a);
+    });
+    // System.out.println("Aluno: " + aluno.get().toString());
+
+    // Tentar cadastrar um aluno sem turma
+    var errorMsgs1 = assertThrows(DomainException.class, () -> {
+      cadastrarAluno.handle(
+        new RegistrarNovoAlunoCommand(
+          "Aluno 2",
+          "360.756.310-11",
+          "782tE@example.com",
+          Set.of()
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs1.get("error").equals("Aluno deve ser matriculado em pelo menos uma turma"), 
+      "deveria apresentar mensagem \"Aluno deve ser matriculado em pelo menos uma turma\""
+    );
+
+    // Tentar cadastrar um aluno em turma inexistente
+    var errorMsgs2 = assertThrows(DomainException.class, () -> {
+      cadastrarAluno.handle(
+        new RegistrarNovoAlunoCommand(
+          "Aluno 3",
+          "360.756.310-11",
+          "782tE@example.com",
+          Set.of(UUID.randomUUID())
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs2.get("error").equals("Aluno deve ser matriculado em pelo menos uma turma"), 
+      "deveria apresentar mensagem \"Aluno deve ser matriculado em pelo menos uma turma\""
+    );
+
+  }
+  
+  @Test
+  void cadastrarAlunoAcimaLotacaoTurma() {
+    // Prepare
+    alunoRepository.deleteAll();
+    turmaRepository.deleteAll();
+
+    assertTrue(5 == Turma.LOTACAO_MAX, "este teste foi configurado para lotação máxima de 5 alunos na turma, porém a lotação atual é " + Turma.LOTACAO_MAX);
+
+    // Cadastrar turma
+    var turma =cadastrarTurma.handle(
+      new CadastrarNovaTurmaCommand(
+        125,
+        2025
+    ));
+
+    // Cadastrar 5 alunos na turma
+    cadastrarAluno.handle(
+      new RegistrarNovoAlunoCommand(
+        "Aluno 1",
+        "540.219.940-09",
+        "782tE@example.com",
+        Set.of(turma.getTurmaId())
+      )
+    );
+
+    cadastrarAluno.handle(
+      new RegistrarNovoAlunoCommand(
+        "Aluno 2",
+        "521.541.110-76",
+        "342tE@example.com",
+        Set.of(turma.getTurmaId())
+      )
+    );
+
+    cadastrarAluno.handle(
+      new RegistrarNovoAlunoCommand(
+        "Aluno 3",
+        "915.302.090-13",
+        "483tE@example.com",
+        Set.of(turma.getTurmaId())
+      )
+    );
+
+    cadastrarAluno.handle(
+      new RegistrarNovoAlunoCommand(
+        "Aluno 4",
+        "695.231.070-38",
+        "896tE@example.com",
+        Set.of(turma.getTurmaId())
+      )
+    );
+
+    cadastrarAluno.handle(
+      new RegistrarNovoAlunoCommand(
+        "Aluno 5",
+        "596.037.800-06",
+        "8f6wZ@example.com",
+        Set.of(turma.getTurmaId())
+      )
+    );
+
+    // Tentar cadastrar o sexto aluno deve dar execação
+    var errorMsgs = assertThrows(DomainException.class, () -> {
+      cadastrarAluno.handle(
+        new RegistrarNovoAlunoCommand(
+          "Aluno 6",
+          "695.231.070-38",
+          "099ti@example.com",
+          Set.of(turma.getTurmaId())
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs.get("error").equals("Lotação da turma excedida"), 
+      "deveria apresentar mensagem \"Lotação da turma excedida\""
+    );
+
+  }
+
+  @Test
+  void cadastrarNovoAlunoDadosInvalidos() {
+    alunoRepository.deleteAll();
+    turmaRepository.deleteAll();
+
+    // Cadastrar turma
+    var turma =cadastrarTurma.handle(
+      new CadastrarNovaTurmaCommand(
+        125,
+        2025
+    ));
+
+    // CPF invalido
+    var errorMsgs1 = assertThrows(DomainException.class, () -> {
+      cadastrarAluno.handle(
+        new RegistrarNovoAlunoCommand(
+          "Aluno 1",
+          "123.456.789-10",
+          "782tE@example.com",
+          Set.of(turma.getTurmaId())
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs1.get("cpf").equals("número do registro de contribuinte individual brasileiro (CPF) inválido"), 
+      "deveria apresentar mensagem \"número do registro de contribuinte individual brasileiro (CPF) inválido\""
+    );
+
+    // Email invalido
+    var errorMsgs2 = assertThrows(DomainException.class, () -> {
+      cadastrarAluno.handle(
+        new RegistrarNovoAlunoCommand(
+          "Aluno 1",
+          "596.037.800-06",
+          "invalid_email",
+          Set.of(turma.getTurmaId())
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs2.get("email").equals("deve ser um endereço de e-mail bem formado"), 
+      "deveria apresentar mensagem \"deve ser um endereço de e-mail bem formado\""
+    );
+  }
+
+  @Test
+  void cadastrarTurmaComDadosInvalidos() {
+    alunoRepository.deleteAll();
+    turmaRepository.deleteAll();
+
+    // Cadastrar turma
+    var turma =cadastrarTurma.handle(
+      new CadastrarNovaTurmaCommand(
+        125,
+        2025
+    ));
+
+    // Tentar cadastrar turma com dados inválidos
+    var errorMsgs = assertThrows(DomainException.class, () -> {
+      cadastrarTurma.handle(
+        new CadastrarNovaTurmaCommand(
+          0,
+          1899
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs.get("anoLetivo").equals("deve ser maior que ou igual à 1900"), 
+      "deveria apresentar mensagem \"deve ser maior que ou igual à 1900\""
+    );
+
+    assertTrue(
+      errorMsgs.get("numeroTurma").equals("deve ser maior que ou igual à 1"), 
+      "deveria apresentar mensagem \"deve ser maior que ou igual à 1\""
+    );
+
+    // Tentar cadastrar turma com mesmo turmaId, porém anoLetivo diferente
+    assertDoesNotThrow(() -> {
+      cadastrarTurma.handle(
+        new CadastrarNovaTurmaCommand(
+          turma.getNumeroTurma(),
+          turma.getAnoLetivo() + 1
+        )
+      );
+    });
+
+    // Tentar cadastrar turma com mesmo turmaId no mesmo anoLetivo
+    var errorMsgs2 = assertThrows(DomainException.class, () -> {
+      cadastrarTurma.handle(
+        new CadastrarNovaTurmaCommand(
+          turma.getNumeroTurma(),
+          turma.getAnoLetivo()
+        )
+      );
+    }).getUserMessages();
+
+    assertTrue(
+      errorMsgs2.get("error").equals("Turma com id "+ turma.getNumeroTurma() +" já cadastrada"), 
+      "deveria apresentar mensagem \"Turma com id "+ turma.getNumeroTurma() +" já cadastrada\""
+    );
+
+    
+  }
+
 }

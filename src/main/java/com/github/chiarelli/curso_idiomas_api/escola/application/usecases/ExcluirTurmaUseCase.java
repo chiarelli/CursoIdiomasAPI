b3e63a98301fef40a0d6375696cdd8eed293e96c
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-10 19:46:19 -0300

    Fixed: "@Transactional" annotation imported from wrong package

--- src/main/java/com/github/chiarelli/curso_idiomas_api/escola/application/usecases/ExcluirTurmaUseCase.java
+++ src/main/java/com/github/chiarelli/curso_idiomas_api/escola/application/usecases/ExcluirTurmaUseCase.java
@@ -3,6 +3,7 @@ package com.github.chiarelli.curso_idiomas_api.escola.application.usecases;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.chiarelli.curso_idiomas_api.escola.domain.InstanceValidator;
 import com.github.chiarelli.curso_idiomas_api.escola.domain.commands.ExcluirTurmaCommand;
@@ -14,7 +15,6 @@ import com.github.chiarelli.curso_idiomas_api.escola.infra.jpa.TurmaRepository;
 import io.jkratz.mediator.core.EventHandler;
 import io.jkratz.mediator.core.Mediator;
 import io.jkratz.mediator.core.RequestHandler;
-import jakarta.transaction.Transactional;
 
 @Component
 public class ExcluirTurmaUseCase implements RequestHandler<ExcluirTurmaCommand, Void> {

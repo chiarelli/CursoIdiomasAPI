eccd5832cfa150081dad1273870abaf18de90489
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-17 15:30:59 -0300

    Defined default sorts for 'Alunos' and 'Turmas'

--- src/main/java/com/github/chiarelli/curso_idiomas_api/escola/application/usecases/ListarAlunosDaTurmaUsecase.java
+++ src/main/java/com/github/chiarelli/curso_idiomas_api/escola/application/usecases/ListarAlunosDaTurmaUsecase.java
@@ -1,5 +1,7 @@
 package com.github.chiarelli.curso_idiomas_api.escola.application.usecases;
 
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.github.chiarelli.curso_idiomas_api.escola.application.queries.ListarAlunosDaTurmaQuery;
 import com.github.chiarelli.curso_idiomas_api.escola.domain.contracts.AlunoInterface;
+import com.github.chiarelli.curso_idiomas_api.escola.domain.model.Aluno;
 import com.github.chiarelli.curso_idiomas_api.escola.infra.jpa.AlunoMapper;
 import com.github.chiarelli.curso_idiomas_api.escola.infra.jpa.TurmaRepository;
 import com.github.chiarelli.curso_idiomas_api.escola.presentation.exceptions.ResourceNotFoundException;
@@ -29,7 +32,10 @@ public class ListarAlunosDaTurmaUsecase implements RequestHandler<ListarAlunosDa
     var turma = repository.findById(query.getTurmaId())
       .orElseThrow(() -> new ResourceNotFoundException("Turma %s nao encontrada".formatted(query.getTurmaId())));
 
-    return turma.getAlunos().stream().map(AlunoMapper::toDomain).collect(Collectors.toSet());
+    return turma.getAlunos().stream()
+        .map(AlunoMapper::toDomain)
+        .sorted(Comparator.comparing(Aluno::getNome))
+        .collect(Collectors.toCollection(LinkedHashSet::new));
   }
 
 }
